83aa8b03cb35be376b83d6995eb9703948be46ae
Ｄａｖｉｄ <davidgj1994@gmail.com>
2017-10-10 14:53:56 +0200

    new changes

--- google_maps_mask_v2.py
+++ google_maps_mask_v2.py
@@ -1,30 +1,60 @@
 
 import numpy as np
 from motionless import CenterMap
-from skimage import io as io2
-
-cmap = CenterMap(lat=38.6038808, lon=-3.4694234, size_x=640, size_y=640, zoom=18, scale=2)
-
-URL = cmap.generate_url()
-URL += "&style=feature:all|element:labels|visibility:off"
-print(URL)
-
-# # req = urlopen('http://answers.opencv.org/upfiles/logo_2.png')
-# # arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-# # print(arr.shape)
-
-# img = io.imread(url)
-# io.imshow(img)
-# io.show()
-
-
+import skimage.io
+from skimage import color
 from PIL import Image
 import urllib.request
 import io
+from skimage import exposure
+
+#39.030236,-3.3761257
+#38.321221,-3.5573312
+#lat=38.6038808, lon=-3.4694234
+cmap = CenterMap(lat=39.030236, lon=-3.3761257, size_x=640, size_y=640, zoom=18, scale=2)
+URL = cmap.generate_url()
+URL += "&style=feature:all|element:labels|visibility:off"
 
 with urllib.request.urlopen(URL) as url:
     f = io.BytesIO(url.read())
 
 img = np.array(Image.open(f))
-io2.imshow(img)
-io2.show()
+skimage.io.imshow(img)
+skimage.io.show()
+
+threshold = 160
+mask = (img > threshold - 10) & (img < threshold + 10) 
+
+# skimage.io.imshow(mask)
+# skimage.io.show()
+
+cmap_sat = CenterMap(lat=39.030236, lon=-3.3761257, maptype='satellite', size_x=640, size_y=640, zoom=18, scale=2)
+img_sat = skimage.io.imread(cmap_sat.generate_url())
+
+skimage.io.imshow(img_sat)
+skimage.io.show()
+
+# img_sat[np.invert(mask)] = [0, 0, 0]
+# skimage.io.imshow(img_sat)
+# skimage.io.show()
+
+alpha = .6
+
+color_mask = np.zeros(img_sat.shape)
+color_mask[mask] = [255, 0, 0]
+skimage.io.imshow(color_mask)
+skimage.io.show()
+
+# color_mask_hsv = color.rgb2hsv(color_mask)
+# img_hsv = color.rgb2hsv(img_sat)
+
+# img_hsv[..., 0] = color_mask_hsv[..., 0]
+# img_hsv[..., 1] = color_mask_hsv[..., 1] * alpha
+
+# img_masked = color.hsv2rgb(img_hsv)
+ 
+blended = alpha * img_sat + (1 - alpha) * color_mask
+blended = exposure.rescale_intensity(blended)
+
+skimage.io.imshow(blended)
+skimage.io.show()
